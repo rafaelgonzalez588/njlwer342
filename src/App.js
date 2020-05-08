5b782d5fd2885e127e1c20c5067f42ab9328d2e3
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-05-07 20:33:27 -0500

    Resolucion del reto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  state = {
+    text: [],
+    inputValue: '',
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState((previous) => {
+      return { ...previous, inputValue: "", text: [ ...previous.text, previous.inputValue ] }
+    })
+  }
+  handleChange = (e) => {
+    let valor = e.target.value;
+    this.setState((previous) => {
+      return { ...previous, inputValue: valor }
+    })
+  }
   render() {
     return (
       <div className="wrapper">
@@ -12,9 +28,16 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.text.map((value, index) => {
+              return <li key={index}>{value}</li>
+            })}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.handleSubmit} >
+             <input type="text" 
+             onChange={this.handleChange} 
+             id="new-task" 
+             placeholder="Ingresa una tarea y oprime Enter" 
+             value={this.state.inputValue}/>
            </form>
         </div>
       </div>
